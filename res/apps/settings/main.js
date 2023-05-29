d2953a1592ce868fa0a2098de88c6df64b674dd6
lumik0 <75566052+lumik0@users.noreply.github.com>
2023-05-29 17:49:24 +0300

    new update

--- res/apps/settings/main.js
+++ res/apps/settings/main.js
@@ -6,6 +6,7 @@ body.append(
     '     <td width="20%">'+
     `         <button id="home" style="width:100%; padding:5px">Home</button>`+
     `         <button id="main" style="width:100%; padding:5px">Main</button>`+
+    `         <button id="main" style="width:100%; padding:5px">Update</button>`+
     '     </td>'+
     '     <td valign="top" width="80%" id="content">'+
     '         This is home'+
@@ -39,7 +40,20 @@ $('#main').on("click", () => {
         ''+
         ''+
         ''+
-        '<button class="metro_btn" onclick="localStorage.clear(); bsod()">Reset</button>'+
+        '<button class="metro_btn"">...</button>'+
+        ''+
+        ''+
+        ''+
+        ''
+    );
+});
+
+$('#update').on("click", () => {
+    $('#content').html(
+        ''+
+        ''+
+        ''+
+        `<button class="metro_btn" onclick="$.ajax({ type: 'GET', url: 'https://raw.githubusercontent.com/lumik0/win10/main/res/update.txt', success: function(data) { fs_readfile('/res/update.txt', (d) => { if(data === d) alert('Это последнее обновление'); else socket.emit('update'); }) } })">Update</button>`+
         ''+
         ''+
         ''+
