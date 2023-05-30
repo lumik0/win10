166f7a935984782925b8c99a57cf1d45e0f9cd1f
lumik0 <75566052+lumik0@users.noreply.github.com>
2023-05-30 18:03:04 +0300

    beta 1.3.3

--- res/apps/finder/main.js
+++ res/apps/finder/main.js
@@ -5,8 +5,8 @@ body.append(
     '  <tbody><tr>'+
     '     <td width="20%">'+
     '         <button id="downloads" style="width:100%; padding:5px">Downloads</button>'+
-    `         <button id="desktop" style="width:100%; padding:5px" onclick="what = 'desktop'; update();">Desktop</button>`+
-    `         <button id="apps" style="width:100%; padding:5px" onclick="what = 'apps'; update();">Apps</button>`+
+    `         <button id="desktop" style="width:100%; padding:5px">Desktop</button>`+
+    `         <button id="apps" style="width:100%; padding:5px">Apps</button>`+
     '         <button id="disk-c" style="width:100%; padding:5px">Disk C</button>'+
     '     </td>'+
     '     <td valign="top" width="80%" id="file">'+
@@ -33,9 +33,8 @@ $('#apps').on( "click", function() {
 var what = '';
 
 function update() {
+    $('#file').html('');
     if(what === 'desktop') {
-        $('#file').html('');
-
         fs_getFilesInDir('/res/desktop', (v) => {
             for(let i = 0; i < v.length; i++) {
                 $('#file').append(`<button onclick="fs_openfile('/res/desktop/`+v[i]+`')" style="height:75px; width: 75px">` +v[i]+`</button> `);
@@ -43,8 +42,6 @@ function update() {
         });
     }
     if(what === 'apps') {
-        $('#file').html('');
-
         fs_getFilesInDir('/res/apps', (v) => {
             for(let i = 0; i < v.length; i++) {
                 $('#file').append(`<button onclick="fs_openfile('/res/apps/`+v[i]+`')" style="height:75px; width: 75px">` +v[i]+`</button> `);
